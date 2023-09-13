d344e2bbd624eb293c94662587958eb19337f575
Adegboyega Ademola <a.adegboyega11@gmail.com>
2023-09-13 14:12:41 -0700

    updated C - Hello, World

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -7,9 +7,6 @@
  */
 int main(void)
 {
-	char *message = "Programming is like building a multilingual puzzle";
-
-	puts(message);
-	return (0);
+    return (0);
 }
 
--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Failure)
+ */
 int main(void)
 {
-    const char *quote = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-
-    /* Write the quote to the standard error */
-    fwrite(quote, sizeof(char), strlen(quote), stderr);
-
-    /* Terminate the program with an error status */
-    return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
